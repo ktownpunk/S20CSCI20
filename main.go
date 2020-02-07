9ff418e1c252b7e41645ba2fca3f150f6371bc7c
ktownpunk <ktownpunkjr.12@gmail.com>
2020-02-07 03:33:14 +0000

    Lab 1.4.1

--- main.go
+++ main.go
@@ -1,30 +1,22 @@
-// Programmer Name: Jacob Harter
-// Date Completed: 1/30/2020
-// Description: Calculate the poulation gain of the U.S. with current population, net gain, and time elapsed
+// Programmer name: Jacob Harter
+// Date completed:  2/6/20
+// Description: variables for my favourite, name, age, prompt to enter them, output them.
 
 package main
 
-import "fmt"
+import "fmt" 
 
-func main() { 
-//
-var Population = 329252103 // population of U.S.
-var NetGain = 24 // rate of growth in people/seconds
-var TimeY = 10 // time in years
-var Time = TimeY*365*24*60*60 // variable of time elapsed in seconds
-var PopulationIncrease = NetGain*Time
-fmt.Println("Current Population of the U.S. is", Population) //state current population
-fmt.Print("The rate of growth is ", NetGain, " people per second") //state growth/time
-fmt.Println(" over", Time, "seconds") //state time elapsed
-fmt.Print("For an increase of ", PopulationIncrease) //state increase
-fmt.Println(" population in",Time,"seconds or",TimeY,"years")// state increase and time in seconds and years
-fmt.Println("The Population of the U.S. in",Time,"seconds or",TimeY, "years will be", Population+PopulationIncrease,"people") // put it all together
-
-//Description: create string variables that hold names, int variables that hold date, put them together for today's date
-var DayOfTheWeek = "Tuesday" //Day of Week
-var Month = "February" //Month
-var Date = 204 // MonthDayDay
-var Year = 2020 //YearYearYearYear
-fmt.Print("Today is 0") // 0 place-holder if month is 1-9
-fmt.Println(Date,"a",DayOfTheWeek,"in",Month,Year) //Statement
+func main() {
+    //declare variable for favorite food and store your favorite food.
+    var favFood = "a nice sandwich"
+    //declare variables for name and age (make sure they are appropriate data types)
+    var name string
+    var age string
+    //ask user to enter name and age
+    fmt.Scanln(&name)
+    fmt.Scanln(&age)
+    //output a welcome statement using their name
+    fmt.Println("Greetings and well-met," + name + ",fellow seeker of truth")
+    //output a statement that says At their age you enjoyed the favorite food
+    fmt.Println("When I was",age,"I enjoyed",favFood)
 }
