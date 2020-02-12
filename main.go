9fb161174b19d6c9d8d802adc42454e7d88069be
ktownpunk <ktownpunkjr.12@gmail.com>
2020-02-12 02:36:38 +0000

    1.2.2 without coments

--- main.go
+++ main.go
@@ -1,14 +1,30 @@
-// Programmer name: Jacob Harter
-// Date completed:  2/11/20
-// Description: calculate inputed distance of rods to feet miles and meters, then display.
+// Programmer Name: Jacob Harter
+// Date Completed: 1/30/2020
+// Description: Calculate the poulation gain of the U.S. with current population, net gain, and time elapsed
 
 package main
 
-import "fmt" 
+import "fmt"
 
 func main() {
-  var rods float
-  fmt.Println("Enter your distance in rods as a decimal")
-  fmt.Scanln(&rods)
-  fmt.Prinln()
+//
+var Population = 329252103 //
+var NetGain = 24 // 
+var TimeY = 10 // 
+var Time = TimeY*365*24*60*60 // 
+var PopulationIncrease = NetGain*Time
+fmt.Println("Current Population of the U.S. is", Population) //
+fmt.Print("The rate of growth is ", NetGain, " people per second") //
+fmt.Println(" over", Time, "seconds") //
+fmt.Print("For an increase of ", PopulationIncrease) //
+fmt.Println(" population in",Time,"seconds or",TimeY,"years")// 
+fmt.Println("The Population of the U.S. in",Time,"seconds or",TimeY, "years will be", Population+PopulationIncrease,"people") // 
+
+//Description: create string variables that hold names, int variables that hold date, put them together for today's date
+var DayOfTheWeek = "Tuesday" //
+var Month = "February" //
+var Date = 204 // 
+var Year = 2020 //
+fmt.Print("Today is 0") //
+fmt.Println(Date,"a",DayOfTheWeek,"in",Month,Year) //
 }
