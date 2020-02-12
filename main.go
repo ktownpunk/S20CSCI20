5859713c06c42f7ad2dd4526ecad036c60b190c8
ktownpunk <ktownpunkjr.12@gmail.com>
2020-02-12 02:34:55 +0000

    1.4.2

--- main.go
+++ main.go
@@ -1,22 +1,14 @@
 // Programmer name: Jacob Harter
-// Date completed:  2/6/20
-// Description: variables for my favourite, name, age, prompt to enter them, output them.
+// Date completed:  2/11/20
+// Description: calculate inputed distance of rods to feet miles and meters, then display.
 
 package main
 
 import "fmt" 
 
 func main() {
-    //declare variable for favorite food and store your favorite food.
-    var favFood = "a nice sandwich"
-    //declare variables for name and age (make sure they are appropriate data types)
-    var name string
-    var age string
-    //ask user to enter name and age
-    fmt.Scanln(&name)
-    fmt.Scanln(&age)
-    //output a welcome statement using their name
-    fmt.Println("Greetings and well-met," + name + ",fellow seeker of truth")
-    //output a statement that says At their age you enjoyed the favorite food
-    fmt.Println("When I was",age,"I enjoyed",favFood)
+  var rods float
+  fmt.Println("Enter your distance in rods as a decimal")
+  fmt.Scanln(&rods)
+  fmt.Prinln()
 }
