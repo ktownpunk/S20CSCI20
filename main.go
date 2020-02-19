ba0f1e5a8dd889710704f07998e4ea8fd507d4a7
ktownpunk <ktownpunkjr.12@gmail.com>
2020-02-19 02:36:20 +0000

    commented project 1

--- main.go
+++ main.go
@@ -8,29 +8,29 @@ import "fmt"
 
 func main() {
 var input string 
-fmt.Println("Enter your temperature scale F, C or K")
-fmt.Scanln(&input)
-if input == "F" {
-  var fahrenheit float32
-  fmt.Println("Enter your temperature")
-  fmt.Scanln(&fahrenheit)
-  var celsius = (fahrenheit-32)*5/9.0
-  var kelvin float32
-  kelvin= (((fahrenheit - 32) * 5/9.0) + 273.15)
-  fmt.Println("Your temperature in Fahrenheit is",fahrenheit,",in Celsius it is",celsius,"and in Kelvin it is",kelvin)
- }else if input == "C"{
-   var celsius float32
-   fmt.Println("Enter your temperature")
-  fmt.Scanln(&celsius)
-  var fahrenheit = (celsius *9/5.0) + 32
-  var kelvin = celsius + 273.15
-  fmt.Println("Your temperature in Fahrenheit is",fahrenheit,",in Celsius it is",celsius,"and in Kelvin it is",kelvin)
- }else if input == "K"{
-   var kelvin float32
-   fmt.Println("Enter your temperature")
-  fmt.Scanln(&kelvin)
-  var celsius = kelvin - 273.15
-  var fahrenheit = (kelvin - 273.15) *9/5.0 + 32
-  fmt.Println("Your temperature in Fahrenheit is",fahrenheit,",in Celsius it is",celsius,"and in Kelvin it is",kelvin)
+fmt.Println("Enter your temperature scale F, C or K") //prompt user to input their temp scale's initial
+fmt.Scanln(&input) //scan and save the input
+if input == "F" { //if it is Fahrenheit
+  var fahrenheit float32 //make float variable fahrenheit
+  fmt.Println("Enter your temperature") //prompt user to ask for their temperature
+  fmt.Scanln(&fahrenheit) //scan and save the input
+  var celsius = (fahrenheit-32)*5/9.0 //F to C
+  var kelvin float32 // make kelvin a variable
+  kelvin= (((fahrenheit - 32) * 5/9.0) + 273.15) //F to K
+  fmt.Println("Your temperature in Fahrenheit is",fahrenheit,",in Celsius it is",celsius,"and in Kelvin it is",kelvin) //print the temp as a number with the corresponding scale
+ }else if input == "C"{ //if C
+   var celsius float32 //make float variable celsius
+   fmt.Println("Enter your temperature") //prompt user to input the temp
+  fmt.Scanln(&celsius) //scan and save the input
+  var fahrenheit = (celsius *9/5.0) + 32 // C to F
+  var kelvin = celsius + 273.15 // C to K
+  fmt.Println("Your temperature in Fahrenheit is",fahrenheit,",in Celsius it is",celsius,"and in Kelvin it is",kelvin) //print the temp as a number with the corresponding scale
+ }else if input == "K"{ //if K
+   var kelvin float32 //make float variable kelvin
+   fmt.Println("Enter your temperature") //prompt user to enter their temp
+  fmt.Scanln(&kelvin) //scan and save input
+  var celsius = kelvin - 273.15 // K to C
+  var fahrenheit = (kelvin - 273.15) *9/5.0 + 32 //K to F
+  fmt.Println("Your temperature in Fahrenheit is",fahrenheit,",in Celsius it is",celsius,"and in Kelvin it is",kelvin) //print temp as a number with its corresponding scale
  }
 }
