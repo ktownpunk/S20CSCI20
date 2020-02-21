5fef16d11518f7a171bc6f1af8c9682dac76570b
ktownpunk <ktownpunkjr.12@gmail.com>
2020-02-21 05:48:31 +0000

    2.1.2 lab part 2

--- main.go
+++ main.go
@@ -1,24 +1,25 @@
 // Programmer name: Jacob Harter
 // Date completed:  2/11/20
 // Description: calculate average of test scores, and make a pascls triangle with a user inputed height.
-
 package main
-
-import "fmt" 
-
-func main() {
-  var totalScore float32 = 0 //totalofScores
-  var count float32 = 0 //amount of inputed scores, not including stop input
-  var input float32 = 0 //value of the input
-  
-fmt.Println("Enter your Score") //prompt and input the score
-fmt.Scanln(&input)
-for input != -1 {
-  totalScore = totalScore + input
-fmt.Println("Enter your Score of -1 to STOP") //prompt another input, either a score or stop
-fmt.Scanln(&input)
-count++ //increase count
-}
-var averageofscores = totalScore/count //take the average of total scores
-fmt.Println("The average is",averageofscores) //print the average
+import "fmt"
+ 
+func main(){
+    var rows int
+    var number int = 1
+    fmt.Print("Enter number of rows : ") //prompt user to input height
+    fmt.Scan(&rows) // input heigh
+    for i := 0; i < rows; i++ {  //loop to be used in formula
+        for k := 0; k <= i; k++ { //loop to be used in formula
+ 
+            if (k==0 || i==0) { //if the loops equal 0 than the number is 1, the sides/first middle
+                    number = 1
+                }else{
+                    number = number*(i-k+1)/k //formula for pascals triangle
+                }
+ 
+            fmt.Print(number) //print the numbers        
+        }
+        fmt.Println(" ") //space the levels
+    }
 }
