3dcbe7689cda67c614e593bcc76f4e6812596181
ktownpunk <ktownpunkjr.12@gmail.com>
2020-02-21 05:15:22 +0000

    2.1.2 lab part 1

--- main.go
+++ main.go
@@ -1,40 +1,24 @@
 // Programmer name: Jacob Harter
-// Date completed:  2/18/2020
-// Description: have the computer count to a user specified number
+// Date completed:  2/11/20
+// Description: calculate average of test scores, and make a pascls triangle with a user inputed height.
 
 package main
 
-import (
-    "fmt"
-    "math/rand"
-  
-) //adding the ability to do random numbers
+import "fmt" 
 
 func main() {
-    var count = 1
-    var userGuess int
-    //create variable for count
-   fmt.Println("Enter the max range")
-    //ask the user to enter a max range for the guessing game and store that value in variable max.
-    var max int
-    fmt.Scanln(&max)
-    fmt.Println(&userGuess)
-    //this next line creates a random number from 1 to that guess for the computer to know.  You can test this by printing out the variable computerGuess
-    var computerGuess = rand.Intn(max)
-
-    //ask the user to enter a guess for the computer number
-    fmt.Println("Enter your guess")
-    fmt.Scanln(&userGuess)
-    //create a loop that compares the computerGuess to the userGuess while they are NOT equal go into the loop
-        //increase the count by 1
-        //tell the user that the guessed incorrect
-        //ask the user to enter a new guess for the computer number
-    for userGuess!=computerGuess{
-      count++
-      fmt.Println("You guessed incorrect")
-      fmt.Println("Try again")
-      fmt.Scanln(&userGuess)
-    }
-    //print out that the user got the answer correctly and how many guesses it took (the count)
-    fmt.Println("You guessed correctly, it took",count,"attempts")
+  var totalScore float32 = 0 //totalofScores
+  var count float32 = 0 //amount of inputed scores, not including stop input
+  var input float32 = 0 //value of the input
+  
+fmt.Println("Enter your Score") //prompt and input the score
+fmt.Scanln(&input)
+for input != -1 {
+  totalScore = totalScore + input
+fmt.Println("Enter your Score of -1 to STOP") //prompt another input, either a score or stop
+fmt.Scanln(&input)
+count++ //increase count
+}
+var averageofscores = totalScore/count //take the average of total scores
+fmt.Println("The average is",averageofscores) //print the average
 }
