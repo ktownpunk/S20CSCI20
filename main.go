f8494ddbc19b65683627a2738d244678b96e983e
ktownpunk <ktownpunkjr.12@gmail.com>
2020-02-26 03:21:15 +0000

    2.3.1 lab Part 1 Updated

--- main.go
+++ main.go
@@ -7,15 +7,17 @@ package main
 import (
     "fmt"
     "math/rand"
-    //"time"
+    "time"
 ) //adding the ability to do random numbers
 
 func main() {
-    var computerchoice = rand.Intn(2)
-    var playerchoice int
+    seed := rand.NewSource(time.Now().UnixNano()) //seed
+    randomNumber := rand.New(seed) //random number
+    var computerchoice = randomNumber.Intn(2) // make the computer's choice random
+    var playerchoice int 
     //create two variables - one for the computer and one for the user
     //use a random integer value representing the computer's choice in a game of Rock, Scissors, Paper. 0=rock, 1=scissors, 2=paper
-    fmt.Println("Enter your choice as 0 (rock) 1 (paper) 2 (scissor)")
+    fmt.Println("Enter your choice as 0 (rock) 1 (paper) 2 (scissor)") 
     fmt.Scanln(&playerchoice)
     //prompt the user for an integer value representing the player's choice
     if computerchoice == 0{
