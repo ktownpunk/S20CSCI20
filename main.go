14b8ee28d89129297b2cebca2c25acae9f0f93b5
ktownpunk <ktownpunkjr.12@gmail.com>
2020-02-28 03:22:50 +0000

    2.3.2 lab part 2

--- main.go
+++ main.go
@@ -6,35 +6,83 @@ package main
 
 import (
     "fmt"
-    "math/rand"
-    "time"
-) //adding the ability to do random numbers
+)
 
 func main() {
-    seed := rand.NewSource(time.Now().UnixNano()) //seed
-    randomNumber := rand.New(seed) //random number
-    var computerchoice = randomNumber.Intn(2) // make the computer's choice random
-    var playerchoice int 
-    //create two variables - one for the computer and one for the user
-    //use a random integer value representing the computer's choice in a game of Rock, Scissors, Paper. 0=rock, 1=scissors, 2=paper
-    fmt.Println("Enter your choice as 0 (rock) 1 (paper) 2 (scissor)") 
-    fmt.Scanln(&playerchoice)
-    //prompt the user for an integer value representing the player's choice
-    if computerchoice == 0{
-      fmt.Println("Computer chooses rock")
-    } else if computerchoice == 1{
-      fmt.Println("Computer chooses paper")
-    } else if computerchoice == 2{
-      fmt.Println("Computer chooses scissors")
-    }
-    if playerchoice == 0{
-      fmt.Println("You chose rock")
-    } else if playerchoice == 1{
-      fmt.Println("You chose paper")
-    } else if playerchoice == 2{
-      fmt.Println("You chose scissors")
-    }
-    
-    //Print out the values using the words rock, scissors, paper.  ie. "Computer chose rock and player chose paper"
-    //You will need to use decisions for this
+ var grossIncome float32 //income variable
+ var status int //status variable
+fmt.Println("Enter your gross income") //prompt input for income
+fmt.Scanln(&grossIncome)
+fmt.Println("Enter your status as a integer 1 for single, 2 for married filing jointed, 3 for head of household, 4 for married filing seperately")//prompt input for status
+fmt.Scanln(&status)
+
+switch status{
+  case 1: { //tax brackets for single
+if grossIncome <= 9700{
+  fmt.Println("You are in the 10% bracket, you owe",grossIncome*.1)
+}else if grossIncome <= 39475{
+  fmt.Println("You are in the 12% bracket, you owe",grossIncome*.12)
+}else if grossIncome <= 84200{
+  fmt.Println("You are in the 22% bracket, you owe",grossIncome*.22)
+}else if grossIncome <= 160725{
+  fmt.Println("You are in the 24% bracket, you owe",grossIncome*.24)
+}else if grossIncome <= 204100{
+  fmt.Println("You are in the 32% bracket, you owe",grossIncome*.32)
+}else if grossIncome <= 510300{
+  fmt.Println("You are in the 35% bracket, you owe",grossIncome*.35)
+}else{
+  fmt.Println("You are in the 37% bracket, you owe",grossIncome*.37) 
+}
+}
+case 2: //tax brackets for married filing jointed
+  if grossIncome <= 19400{
+  fmt.Println("You are in the 10% bracket, you owe",grossIncome*.1)
+}else if grossIncome <= 78950{
+  fmt.Println("You are in the 12% bracket, you owe",grossIncome*.12)
+}else if grossIncome <= 168400{
+  fmt.Println("You are in the 22% bracket, you owe",grossIncome*.22)
+}else if grossIncome <= 321450{
+  fmt.Println("You are in the 24% bracket, you owe",grossIncome*.24)
+}else if grossIncome <= 408200{
+  fmt.Println("You are in the 32% bracket, you owe",grossIncome*.32)
+}else if grossIncome <= 612350{
+  fmt.Println("You are in the 35% bracket, you owe",grossIncome*.35)
+}else{
+  fmt.Println("You are in the 37% bracket, you owe",grossIncome*.37) 
+}
+
+  case 3: //tax brackets for head of household
+  if grossIncome <= 13850{
+  fmt.Println("You are in the 10% bracket, you owe",grossIncome*.1)
+}else if grossIncome <= 52850{
+  fmt.Println("You are in the 12% bracket, you owe",grossIncome*.12)
+}else if grossIncome <= 84200{
+  fmt.Println("You are in the 22% bracket, you owe",grossIncome*.22)
+}else if grossIncome <= 160700{
+  fmt.Println("You are in the 24% bracket, you owe",grossIncome*.24)
+}else if grossIncome <= 204100{
+  fmt.Println("You are in the 32% bracket, you owe",grossIncome*.32)
+}else if grossIncome <= 510300{
+  fmt.Println("You are in the 35% bracket, you owe",grossIncome*.35)
+}else{
+  fmt.Println("You are in the 37% bracket, you owe",grossIncome*.37) 
+}
+  case 4: //tax brackets for married filing seperately
+  if grossIncome <= 9700{
+  fmt.Println("You are in the 10% bracket, you owe",grossIncome*.1)
+}else if grossIncome <= 39475{
+  fmt.Println("You are in the 12% bracket, you owe",grossIncome*.12)
+}else if grossIncome <= 84200{
+  fmt.Println("You are in the 22% bracket, you owe",grossIncome*.22)
+}else if grossIncome <= 160725{
+  fmt.Println("You are in the 24% bracket, you owe",grossIncome*.24)
+}else if grossIncome <= 204100{
+  fmt.Println("You are in the 32% bracket, you owe",grossIncome*.32)
+}else if grossIncome <= 306175{
+  fmt.Println("You are in the 35% bracket, you owe",grossIncome*.35)
+}else{
+  fmt.Println("You are in the 37% bracket, you owe",grossIncome*.37) 
+}
+
+}
 }
