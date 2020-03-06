9c2bdf599997eff3f75c73913ef93479a78a2bdf
ktownpunk <ktownpunkjr.12@gmail.com>
2020-03-06 06:26:01 +0000

    2.5.2 Lab Complete

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ package main
 
 import (
     "fmt"
+    "time"
+    "math/rand"
 )
 func main() {
  var column [3] string
@@ -50,16 +52,56 @@ func main() {
             row3[userColumn] = "X"
           }
           }
- //}    
+ 
     
 
 fmt.Println(row)
    fmt.Println(row2)
     fmt.Println(row3)
    }
-   }
-  
-
+   //ID lookup by name
+   var ID [5] string
+   var userName string
+   var counter int
+   var IDFound = false
+   ID[0] = "John"
+   ID[1] = "James"
+   ID[2] = "Luke"
+   ID[3] = "Sam"
+   ID[4] = "Jessie"
+fmt.Println("Enter Name (Case Sensitive)")
+fmt.Scanln(&userName)
+for counter <6 {
+  if userName == ID[counter] {
+  fmt.Println("Your ID is", counter)
+  IDFound = true
+  counter++
+  }else if IDFound == false{
+    fmt.Println("The name you entered is not in our system")
+    break
+  }
+}
+seed := rand.NewSource(time.Now().UnixNano())
+    randomNumber := rand.New(seed)
+var numbers = [5]int{(randomNumber.Intn(5))}
+	min, max := findMinAndMax(numbers)
+	fmt.Println("Minimum: ", min) //prints the min value
+	fmt.Println("Maximum: ", max) //print the max value
+  fmt.Println(numbers)
+}
+func findMinAndMax(numbers [5]int) (min int, max int) { //function to find the min and max from the array "numbers"
+	min = numbers[0] //sets min
+	max = numbers[0] //sets max
+	for _, value := range numbers { //for each value compare the number to value
+		if value < min { //checks the min to another value
+			min = value //sets value to the smaller one
+		}
+		if value > max { //checks the max to another value
+			max = value //sets value to the greater one
+		}
+	}
+	return min, max //gives the minimum and maximum values
+}
 
 
  
