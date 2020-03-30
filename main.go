7f8cec1693b331223c8301822e6de0827d1d339c
ktownpunk <ktownpunkjr.12@gmail.com>
2020-03-30 04:51:49 +0000

    3.2.1 Lab Part 1

--- main.go
+++ main.go
@@ -1,73 +1,19 @@
 // Programmer name: Jacob Harter
-// Date completed:  3/05/2020
-// Description: Pig
+// Date completed:  3/29/20
+// Description: ASCII Art
 
 package main
 
-import (
-    "fmt"
-    "time"
-    "math/rand"
-)
+import "fmt" 
+func Art(){ //My function for the "art"
+  fmt.Println("0        0    000000000")
+  fmt.Println("0        0        0    ")
+  fmt.Println("0        0        0    ")
+  fmt.Println("0000000000        0    ")
+  fmt.Println("0        0        0    ")
+  fmt.Println("0        0        0    ")
+  fmt.Println("0        0    000000000")
+}
 func main() {
- var computerScore int //computer's score
- var userScore int //user's score
- var turnCounter =99 //monitors turns
- var dice int //stores dice max value
- var currentRoll int //short term value hold
- var pot int //point pot
- var answer string //used for user's choice
- var choice int //used for computer's choice
- seed := rand.NewSource(time.Now().UnixNano()) //seed for random#
- roll := rand.New(seed) //roll is the random#
- fmt.Println("What is the max value of the dice")
- fmt.Scanln(&dice)
- for userScore<100 && computerScore<100{
-   switch turnCounter{
-     case 99: //user's turn
-      currentRoll =(roll.Intn(dice)) +1 //roll
-      fmt.Println(currentRoll) //print the roll
-      if currentRoll == 1 { 
-        fmt.Println("Your turn is over")
-        turnCounter++ //pass turn
-        }else if currentRoll!=1{
-      pot = pot + currentRoll //add roll to pot
-      fmt.Println("Type roll to roll again, type anything else to end") //ask if User wants to roll again
-      fmt.Scanln(&answer)
-        if answer == "roll"{
-      pot = pot + currentRoll
-        }else{
-        userScore = userScore + pot
-        fmt.Println("Your score is ",userScore)
-        pot = 0
-        turnCounter++
-      }
-
-        }
-        case 100: //computer's turn
-        currentRoll =(roll.Intn(dice)) +1
-      fmt.Println(currentRoll)
-      if currentRoll == 1{
-        fmt.Println("The computer's turn is over")
-        turnCounter--
-        }else if currentRoll != 1{
-          pot = pot + currentRoll
-          choice = (roll.Intn(6))
-          if choice != 5{
-      pot = pot + currentRoll
-          }else{
-          computerScore = computerScore + pot
-        fmt.Println("Computer score is ",computerScore)
-        pot = 0
-        turnCounter--
-          }
-          
-        }
-      }
-}
-if computerScore >= 100{
-fmt.Println("Computer Wins")
-}else if userScore >=100{
-  fmt.Println("You Win!")
-}
+  Art()//calls the function, displaying the "art"
 }
