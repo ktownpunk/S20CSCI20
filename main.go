0d84a263f0b50f152c9e810b200ccb440f24921f
ktownpunk <ktownpunkjr.12@gmail.com>
2020-03-30 05:23:23 +0000

    3.2.1 Lab Part 2

--- main.go
+++ main.go
@@ -1,19 +1,48 @@
 // Programmer name: Jacob Harter
 // Date completed:  3/29/20
-// Description: ASCII Art
+// Description:Random feedback
 
 package main
 
-import "fmt" 
-func Art(){ //My function for the "art"
-  fmt.Println("0        0    000000000")
-  fmt.Println("0        0        0    ")
-  fmt.Println("0        0        0    ")
-  fmt.Println("0000000000        0    ")
-  fmt.Println("0        0        0    ")
-  fmt.Println("0        0        0    ")
-  fmt.Println("0        0    000000000")
+import "fmt"
+import "math/rand"
+import "time"
+func Correct(){ //correct function, makes a random numebr which the switch outputs a random phrase
+seed := rand.NewSource(time.Now().UnixNano())
+random := rand.New(seed)
+var feedback = (random.Intn(3))
+  switch feedback{
+    case 0:fmt.Println("Very Good!")
+    case 1:fmt.Println("Nice Job!")
+    case 2:fmt.Println("Keep it up!")
+  }
+  
+
 }
-func main() {
-  Art()//calls the function, displaying the "art"
+func Incorrect(){//incorrect funtion, random number, switch same as the previous function
+seed2 := rand.NewSource(time.Now().UnixNano())
+random2 := rand.New(seed2)
+var feedback = (random2.Intn(3))
+  switch feedback{
+    case 0:fmt.Println("Try Again")
+    case 1:fmt.Println("Don't give up!")
+    case 2:fmt.Println("Give it another shot!")
+  }
+
+}
+
+func main() {//asks user the question and displays the choices to answer, then prompts input
+  var UserInput string
+  fmt.Println("What is a variable")
+  fmt.Println("A. A reserved place in memory which can store one value")
+  fmt.Println("B. A named number")
+  fmt.Println("C. A math term")
+  fmt.Println("D. A series of characters")
+  fmt.Println("Enter your answer as a capital single character A B C or D")
+  fmt.Scanln(&UserInput)
+  if UserInput == "A" { //checks if the input is true or false
+  Correct() //if true, calls on the correct function
+  }else{
+  Incorrect()//if false calls on the incorrect function
+  }
 }
