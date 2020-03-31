42fd9af9e9e6f5d59a37e27452b94f0ed72244e1
ktownpunk <ktownpunkjr.12@gmail.com>
2020-03-31 00:35:46 +0000

    Project WIP

--- main.go
+++ main.go
@@ -1,35 +1,32 @@
 package main
 
-import (
-    "fmt"
-)
-func chorus(){ 
-  fmt.Println("And now I'm turning to dust in a world that's twisted")
-  fmt.Println("Don't come searching when I go missing")
-  fmt.Println("Close your eyes or just try to look away... I don't want to hurt you")
-  fmt.Println("We live in a world someone else imagined, the ghost of what's left of me is all but vanished")
-  fmt.Println("Remember by heart, how bright I used to shine")
-}
+import(
+"fmt"
+"os"
+//"bufio"
+"strconv"
 
+)
 func main() {
-  fmt.Println("Oh can you tell me, can you tell me the way the story ends.")
-  fmt.Println("A monster in my heart, a ghost inside my chest")
-  fmt.Println("I'm broken down the world around me surrounds my suffering,you smile and laugh at me, but you don't see a thing")
-  fmt.Println("-pause-")
-  fmt.Println("Damaged and broken as I am, I'm trying not to breathe.")
-  fmt.Println("Unravel I'm, not unraveled by the truth I finally see... freeze.")
-  fmt.Println("I'm breakable. Unbreakable. I'm shakeable, unshakeable,unraveling since I found you.")
-  chorus()
-  fmt.Println("Entangled in the lonlieness, the memories of innocence")
-  fmt.Println("It's stinging me, It's breaking me, the pain is spreading endlessly")
-  fmt.Println("I cannot move, I close my eyes I try to breath I realize, I'm paralyzed, I'm paralyzed...UNRAVEL THE WORLD")
-  fmt.Println("I'm not what I was then, don't touch the infection. Entwined we will both die so stay away, stay alive")
-  fmt.Println("I'm breakable,unbreakable, I'm shakeable. Unshakeable. Unravelling I won't infect you!")
-  chorus()
-  fmt.Println("Please, just don't forget me,don;t forget me, don't forget me, don't forget me,don't forget me!")
-  fmt.Println("We live in a world someone else imagined")
-  fmt.Println("The ghost of what's left of me all but vanished")
-  fmt.Println("Remember my heart, how bright I used to shine")
-  fmt.Println("Oh can you tell me, oh can you tell me")
-  fmt.Println("A monster in my heart, and now there's nothing left.")
+  var userInput int
+  var YN string
+  var gNumber int
+  Stats, err := os.Create("Stats.txt")
+  if err != nil{
+    panic(err)
+  }
+  fmt.Println("Enter Y to continue or N to Stop")
+  fmt.Scanln(&YN)
+  for YN == "Y"{
+    Stats.WriteString(strconv.Itoa(gNumber))
+    Stats.WriteString("/n")
+    fmt.Println("Enter the damage for this test")
+    fmt.Scanln(&userInput)
+    Stats.WriteString("/n")
+    fmt.Println("Enter Y to continue, N to stop")
+    fmt.Scanln(&YN)
+  }
+  
+  Stats.Sync()
+  Stats.Close()
 }
