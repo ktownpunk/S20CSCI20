c47ff552ea9a553cac99208985f2db73903f14b7
ktownpunk <ktownpunkjr.12@gmail.com>
2020-04-02 22:35:23 +0000

    3.1 Activity, Code Updated.

--- main.go
+++ main.go
@@ -1,19 +1,44 @@
-// Programmer name: Jacob Harter
-// Date completed:  3/29/20
-// Description:Random feedback
-
 package main
 
-import "fmt"
-//import "math/rand"
-//import "time"
-func greeting(userInput string){ 
-fmt.Println("Greetings,"+userInput)
+import(
+"fmt"
+)
+
+func addNumbers(input1 int, input2 int){
+ fmt.Println(input1, "+", input2, "=", input1+input2)
 }
 
+func subNumbers(input1 int, input2 int){
+ fmt.Println(input1, "-", input2, "=", input1-input2)
+}
+
+func divNumbers(input1 int, input2 int){
+  fmt.Println(input1, "/",input2,"=",input1/input2)
+}
+func mulNumbers(input1 int, input2 int){
+  fmt.Println(input1,"*",input2,"=",input1*input2)
+}
 func main() {
-  var userInput string
-  fmt.Println("Enter your name")
-  fmt.Scanln(&userInput)
-  greeting(userInput)
-  }
+var num1, num2 int
+var op string
+
+ fmt.Println("Please enter a whole number between 1 and 10")
+ fmt.Scan(&num1)
+ fmt.Println("Please enter another whole number between 1 and 10")
+ fmt.Scan(&num2)
+
+ fmt.Println("Please enter a mathematical operator (+,-,/,*)")
+ fmt.Scan(&op)
+
+if(op == "+"){
+ addNumbers(num1,num2)
+ } else if(op == "-"){
+ subNumbers(num1,num2)
+ }else if (op == "/"){
+ divNumbers(num1,num2)
+ }else if (op == "*"){
+ mulNumbers(num1,num2)
+ }else{
+     fmt.Println("Invalid Operator")
+   }
+ }
